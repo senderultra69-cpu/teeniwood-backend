f430f4dcdb728fb8890f4cb4e9c02b170156a061
senderultra69-cpu <senderultra69@gmail.com>
2026-06-05 02:21:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.post("/api/generate", async (req, res) => {
     // ================= GEMINI =================
     if (engine === "gemini") {
       const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash-latest"
+        model="gemini-2.5-flash"
       });
 
       const result = await model.generateContent(
